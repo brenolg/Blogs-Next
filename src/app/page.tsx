d565dd4e42d5ed6b1b2ca7aa373bf00966a7201f
brenolg <brenolgar@gmail.com>
2025-10-07 15:41:47 -0300

    simulate loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-
+import { PostsList } from "@/components/PostList";
+import { SpinLoader } from "@/components/SpinLoader";
+import { Suspense } from "react";
 
 export default function HomePage() {
   return (
-    <div>x</div>
+    <div>
+      <header>
+        <h1 className="text-6xl font-bold text-center py-8">Aqui é a HEADER</h1>
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer>
+        <p className="text-6xl font-bold text-center py-8">Footer</p>
+      </footer>
+    </div>
   );
 }
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -11,8 +11,14 @@ const JSON_POSTS_FILE_PATH = resolve(
   "seed",
   "posts.json"
 );
-
+const SIMULATE_WAIT_IN_MS = 5000;
 export class JsonPostRepository implements PostRepository {
+  private async simulateWait() {
+    if (SIMULATE_WAIT_IN_MS <= 0) return;
+
+    await new Promise((resolve) => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
+  }
+
   private async readFromDisk(): Promise<PostModel[]> {
     const fileContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
     const parsedJson = JSON.parse(fileContent) as { posts: PostModel[] };
@@ -20,11 +26,15 @@ export class JsonPostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> {
+    await this.simulateWait();
+
     const posts = await this.readFromDisk();
     return posts;
   }
 
   async findById(id: string): Promise<PostModel> {
+    await this.simulateWait();
+
     const posts = await this.findAll();
     const post = posts.find((post) => post.id === id);
 
